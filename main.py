935ef3d2ad7284b78ab7f0fec639868c6ad16a1c
OX-S <48330226+OX-S@users.noreply.github.com>
2020-07-16 02:41:48 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+import discord
+import random
+from discord.ext import commands
+
+token = "       "
+
+bot = commands.Bot(command_prefix=("!"), self_bot=True)
+
+@bot.event
+async def on_ready():
+    print("Bot presence t u r n e d on ( ͡° ͜ʖ ͡°)")
+
+@bot.command(pass_context=True)
+async def e(ctx, message):
+	content = ctx.message.content[3:]
+	color = discord.Color(value=int("%06x" % random.randint(0, 0xFFFFFF), 16))
+	embedvar = discord.Embed(description=content, color=color)	
+	await ctx.message.delete()
+	await ctx.send(embed=embedvar)
+
+bot.run(token, bot=False)
+
